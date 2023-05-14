a785aa68512e2c5fe6abb3b61fb879bf5451fdda
Jyoti Ranjan Kumar <kumar.jyotiranjan575@gmail.com>
2023-05-14 21:32:44 +0530

    demo check

--- src/main/java/com/driver/controllers/HotelManagementController.java
+++ src/main/java/com/driver/controllers/HotelManagementController.java
@@ -27,7 +27,7 @@ public class HotelManagementController
     public String addHotel(@RequestBody Hotel hotel)
     {
 
-        //You need to add an hotel to the database
+        //You need to add an hotel to the databas
         //incase the hotelName is null or the hotel Object is null return an empty a FAILURE
         //Incase somebody is trying to add the duplicate hotelName return FAILURE
         //in all other cases return SUCCESS after successfully adding the hotel to the hotelDb.
